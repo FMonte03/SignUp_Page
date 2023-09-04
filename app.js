74d1a148358869b1fb1e3c41a0088de6f8dd262e
Filippo <122409227+FMonte03@users.noreply.github.com>
2023-09-04 13:42:42 -0400

    added password strength checker, password matching checker, and finally made it so form will not be submitted if passwords don't match and do not follow the requirements

--- app.js
+++ app.js
@@ -12,3 +12,69 @@ input.nextElementSibling.classList.remove('filled');
 } )
 
 
+
+
+//matching passwords
+//and password strength
+
+const cpassword = document.querySelector('#cpass')
+const hint = document.querySelector('.hint')
+
+const password = document.querySelector('#pass'); 
+password.addEventListener('input',() => {
+    if(!isStrongPassword(password.value)){
+        hint.classList.remove('correct')
+        hint.classList.add('incorrect')
+    }
+    else{
+        hint.classList.remove('incorrect')
+        hint.classList.add('correct')
+    }
+})
+
+const matchingMessage = document.querySelector('.match')
+
+function validate(event){
+    if((!isPasswordsMatch())||(!isStrongPassword(password.value))){
+        event.preventDefault();        
+        return false;
+    }
+}
+
+
+cpassword.addEventListener('input', () => {
+    if(!isPasswordsMatch()){
+        matchingMessage.innerHTML = "Passwords do not match!"
+        matchingMessage.classList.remove('correct')
+    }
+    else{
+        matchingMessage.classList.add('correct')
+        matchingMessage.innerHTML= "Passwords match!"
+    }
+
+})
+
+function isPasswordsMatch(){
+    if(password.value != cpassword.value){
+        return false;
+    }
+    else{
+        return true;
+    }
+}
+
+
+function isStrongPassword(password){
+    const isContainsUppercase = /^(?=.*[A-Z])/;
+    const isContainsLowercase = /^(?=.*[a-z])/;
+    const isContainsNumber = /^(?=.*[0-9])/;
+    const isContainsSymbol =  /^(?=.*[~`!@#$%^&*()--+={}\[\]|\\:;"'<>,.?/_₹])/;
+
+    if(isContainsLowercase.test(password) && isContainsNumber.test(password) && isContainsSymbol.test(password) && isContainsUppercase.test(password))
+    {
+    
+        return true; 
+    }
+    return false;
+
+}
